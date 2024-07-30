fc79b962ea5b226ee6e2d7cd92aea284244b1c98
Antal Lohár <a123lohar@gmail.com>
2024-07-31 00:19:57 +0200

    Wrong Password test

---
+++ cypress/e2e/wrong_password_test.cy.js
@@ -0,0 +1,19 @@
+/// <reference types="cypress" />
+
+describe('Bejelentkezési teszt', () => {
+    it('Rossz jelszóval történő bejelentkezés', () => {
+      cy.visit('https://hu.wikipedia.org/wiki/Kezd%C5%91lap')
+  
+      cy.get('#pt-login-2 > a > span').click()
+
+    cy.get('#wpName1').type('Antaltesztelo')
+    cy.get('#wpPassword1').type('Tesztelo011')
+
+    cy.get('#wpLoginAttempt').click()
+
+    cy.wait(2000)
+  
+      
+      cy.contains('jelszó érvénytelen').should('be.visible')
+    })
+  })
