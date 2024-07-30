253a4fc3617736a7fdeffcd1a8660b6e1a0458af
Antal Lohár <a123lohar@gmail.com>
2024-07-30 14:54:00 +0200

    Add to Cypress Cloud

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    projectId: "4756du",
     watchForFileChanges: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
